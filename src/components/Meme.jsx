e553829fcd94a7fb82fca741e7d1229092f97c49
Joseph Monakedi <99967046+TraderJoe97@users.noreply.github.com>
2024-11-16 09:17:29 +0200

    handle input chage

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -4,21 +4,26 @@ export default function Meme() {
     const [meme,setMeme] = useState({
         url:"http://i.imgflip.com/1bij.jpg",
         topText: "",
-        BottomText: "",})
+        bottomText: "",})
     async function getMemeImage() {
         const response = await fetch('https://api.imgflip.com/get_memes');
         const data = await response.json();
         const randomMeme = data.data.memes[Math.floor(Math.random() * data.data.memes.length)];
         setMeme(PrevMeme => ({...PrevMeme, url: randomMeme.url}));
     }
+
+    function handleChange(event){
+        const {name, value} = event.target;
+        setMeme(prevMeme => ({...prevMeme, [name]: value}));
+    }
     return (
         <main className="meme--container"> 
             <div className="form">
                 <label htmlFor="top-text">Top Text
-                    <input id="top-text" type="text" placeholder="shut up" className="form--input"></input>
+                    <input onChange={handleChange} name="topText" value={meme.topText} id="top-text" type="text" placeholder="shut up" className="form--input"></input>
                 </label>
                 <label htmlFor="top-text">Bottom Text
-                <input type="text" placeholder="and take my money" className="form--input"></input>
+                <input onChange={handleChange} name="bottomText" value={meme.bottomText} type="text" placeholder="and take my money" className="form--input"></input>
                 </label>
                 <button onClick={getMemeImage} className="form--button">Get a new meme image 🖼</button>
                 <div className="meme">
