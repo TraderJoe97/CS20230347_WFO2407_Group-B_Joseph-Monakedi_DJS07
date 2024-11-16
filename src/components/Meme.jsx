151f3ce7c698a267b6e08084be1e7b1ecb79e533
Joseph Monakedi <99967046+TraderJoe97@users.noreply.github.com>
2024-11-16 08:37:13 +0200

    get random meme url

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,4 +1,16 @@
+import { useState } from "react";
+
 export default function Meme() {
+    const [meme,setMeme] = useState({
+        url:"http://i.imgflip.com/1bij.jpg",
+        topText: "",
+        BottomText: "",})
+    async function getMemeImage() {
+        const response = await fetch('https://api.imgflip.com/get_memes');
+        const data = await response.json();
+        const randomMeme = data.data.memes[Math.floor(Math.random() * data.data.memes.length)];
+        setMeme(PrevMeme => ({...PrevMeme, url: randomMeme.url}));
+    }
     return (
         <main className="meme--container"> 
             <div className="form">
@@ -8,7 +20,14 @@ export default function Meme() {
                 <label htmlFor="top-text">Bottom Text
                 <input type="text" placeholder="and take my money" className="form--input"></input>
                 </label>
-                <button className="form--button">Get a new meme image 🖼</button>
+                <button onClick={getMemeImage} className="form--button">Get a new meme image 🖼</button>
+                <div className="meme">
+                    <div className="meme--text">
+                        <p className="top">Top Text</p>
+                        <p className="bottom">Bottom Text</p>
+                    </div>
+                    <img className="meme--image" src={meme.url} alt="meme"/>
+                </div>
             </div>
         </main>
     );
